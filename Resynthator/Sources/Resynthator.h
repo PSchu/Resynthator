09aba1692b45a092adc6a1954ee67c5de1155275
Peter Schumacher <pschu@me.com>
2016-06-22 20:22:27 +0200

    Created base ground for Framework

---
+++ Resynthator/Sources/Resynthator.h
@@ -0,0 +1,19 @@
+//
+//  Resynthator iOS.h
+//  Resynthator iOS
+//
+//  Created by Peter Schumacher on 22.06.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Resynthator iOS.
+FOUNDATION_EXPORT double ResynthatorVersionNumber;
+
+//! Project version string for Resynthator iOS.
+FOUNDATION_EXPORT const unsigned char ResynthatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Resynthator_iOS/PublicHeader.h>
+
+
---
+++ Resynthator/Sources/Resynthator.swift
@@ -0,0 +1,9 @@
+//
+//  Resynthator.swift
+//  Resynthator
+//
+//  Created by Peter Schumacher on 22.06.16.
+//
+//
+
+import Foundation
