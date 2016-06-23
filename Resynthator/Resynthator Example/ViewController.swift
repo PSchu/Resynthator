6d968fe1ded16f24781dd16ab4ea2edfcad7cbf9
Peter Schumacher <Peter.Schumacher@burda-forward.de>
2016-06-23 10:04:59 +0200

    hopefully fixed it

--- Resynthator/Resynthator Example/ViewController.swift
+++ Resynthator/Resynthator Example/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Resynthator
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Resynthator.printStuff()
     }
 
     override func didReceiveMemoryWarning() {
